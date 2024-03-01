554213cc228d684e35e06e7ddcb0397dd4989ce1
KaitouKiddoVFX <prometheus1204@gmail.com>
2024-03-01 13:03:11 +0800

    Add GLFW WindowsWindow

--- Tide/src/Tide/Log.cpp
+++ Tide/src/Tide/Log.cpp
@@ -1,4 +1,4 @@
-#include "Log.h"
+#include "tdpch.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 
 namespace Tide
--- Tide/src/Tide/TideApp.cpp
+++ Tide/src/Tide/TideApp.cpp
@@ -1,13 +1,27 @@
+#include "tdpch.h"
 #include "TideApp.h"
+#include "Tide/Events/AppEvent.h"
+#include "Tide/Log.h"
+#include <GLFW/glfw3.h>
 
 namespace Tide
 {
-	TideApp::TideApp() { };
-	TideApp::~TideApp() { };
-	void TideApp::Run() { 
-		for (int i = 0; i < 10; i++)
+	TideApp::TideApp()
+	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
+	};
+
+	TideApp::~TideApp()
+	{
+	};
+
+	void TideApp::Run()
+	{ 
+		while (m_Running)
 		{
-			//std::cout << "Fuck" << "\n";
+			//glClearColor(1, 0, 1, 1);
+			//glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
 		}
 	};
 }
