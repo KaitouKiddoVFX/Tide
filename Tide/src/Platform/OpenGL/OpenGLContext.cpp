14e7bbc18357d96fb00ea64889665611eddf601e
kaitoukiddovfx <prometheus1204@gmail.com>
2024-06-10 06:36:51 +0800

    Frist Triangle

--- Tide/src/Platform/OpenGL/OpenGLContext.cpp
+++ Tide/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,15 @@ namespace Tide
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		TD_CORE_ASSERT(status, "Failed to initialze Glad!");
+
+		// Temporary
+		TD_CORE_INFO("OpenGL Info");
+		std::cout << "Vendor " << glGetString(GL_VENDOR) << std::endl;
+		std::cout << "Renderer " << glGetString(GL_RENDERER) << std::endl;
+		std::cout << "Version " << glGetString(GL_VERSION) << std::endl;
+		// TD_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		// TD_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		// TD_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
