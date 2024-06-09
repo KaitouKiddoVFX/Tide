b56439620b3a160e1a21f72bba9386ee90015ef7
kaitoukiddovfx <prometheus1204@gmail.com>
2024-06-10 05:55:40 +0800

    Add OpenGLContext Class

--- Tide/src/Platform/Windows/WindowsWindow.cpp
+++ Tide/src/Platform/Windows/WindowsWindow.cpp
@@ -5,7 +5,7 @@
 #include "Tide/Events/KeyEvent.h"
 #include "Tide/Events/MouseEvent.h"
 
-#include <glad/glad.h>
+#include "Platform/OpenGL/OpenGLContext.h"
 
 namespace Tide
 {
@@ -49,9 +49,9 @@ namespace Tide
 		}
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-		glfwMakeContextCurrent(m_Window);
-		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-		TD_CORE_ASSERT(status, "Failed to initialize Glad!");
+		m_Context = new OpenGLContext(m_Window);
+		m_Context->Init();
+
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
@@ -152,7 +152,7 @@ namespace Tide
 	void WindowsWindow::OnUpdate()
 	{
 		glfwPollEvents();
-		glfwSwapBuffers(m_Window);
+		m_Context->SwapBuffers();
 	}
 
 	void WindowsWindow::SetVSync(bool enabled)
