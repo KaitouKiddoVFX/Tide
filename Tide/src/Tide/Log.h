5f13b54a9ccf9e07ed97df5faf05aee1bed9d390
kaitoukiddovfx <prometheus1204@gmail.com>
2024-06-23 18:04:36 +0800

    Filter vcxproj files

--- Tide/src/Tide/Log.h
+++ Tide/src/Tide/Log.h
@@ -1,7 +1,7 @@
 #pragma once
+#include <memory>
 #include "Core.h"
 #include "spdlog/spdlog.h"
-#include <memory>
 
 namespace Tide
 {
@@ -11,6 +11,7 @@ namespace Tide
 		static void Init();
 		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
 		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
 	private:
 		static std::shared_ptr<spdlog::logger> s_CoreLogger;
 		static std::shared_ptr<spdlog::logger> s_ClientLogger;
