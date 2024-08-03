3b81a05ed2e09290594fa60ff4578445bdc3e4ef
kaitoukiddovfx <prometheus1204@gmail.com>
2024-08-03 23:24:50 +0800

    Added Proper Window Resize Handling

--- Tide/src/Tide/Renderer/Renderer.h
+++ Tide/src/Tide/Renderer/Renderer.h
@@ -10,6 +10,7 @@ namespace Tide
 	{
 	public:
 		static void Init();
+		static void OnWindowResize(uint32_t width, uint32_t height);
 		static void BeginScene(OrthographicCamera& camera);
 		static void EndScene();
 		static void Submit(const Tide::Ref<Shader>& shader, const Tide::Ref<VertexArray>& vertexArray, const glm::mat4& transform = glm::mat4(1.0f));
--- Tide/src/Tide/TideApp.h
+++ Tide/src/Tide/TideApp.h
@@ -23,14 +23,17 @@ namespace Tide
 		inline static TideApp& Get() { return *s_Instance; }
 
 	private:
+		bool OnWindowResize(WindowResizeEvent& e);
 		bool OnWindowClose(WindowCloseEvent& e);
 
 	private:
 		std::unique_ptr<Window> m_Window;
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
+		bool m_Minimized = false;
 		LayerStack m_LayerStack;
 		float m_LastFrameTime = 0.0f;
+
 	private:
 		static TideApp* s_Instance;
 	};
